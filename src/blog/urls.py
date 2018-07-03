a9d8aeee4cecec4e962f7e6a007dc7f14461e625
Douglas Mendizábal <douglas@redrobot.io>
2018-07-02 19:52:17 -0500

    Removed django source

--- src/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import urls
-
-from . import views
-
-
-urlpatterns = [
-    urls.url(r'^$', views.index, name='index'),
-]
--- src/blog/views.py
+++
@@ -1,7 +0,0 @@
-from django import http
-from django.shortcuts import render
-
-
-# Create your views here.
-def index(request):
-    return http.HttpResponse("Hello Alamo City Python!")
